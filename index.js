8115469b87f17d003aa56db76f3bd24bcb7c2db6
Zoya Yusuf <zoyayusuf04@gmail.com>
2024-09-26 20:50:37 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-i
+const mongoose = require("mongoose");
+const listing = require("../models/listing.js");
+const initData = require("./data.js");
+
+const Mongo_url="mongodb://127.0.0.1:27017/wanderlust"; //will be created when a colection is added in it
+
+main()
+    .then(()=>{
+        console.log("connected");
+    })
+    .catch((err)=>{
+        console.log("error");
+        
+    })
+
+//connect to mongodb
+async function main() {
+    await mongoose.connect(Mongo_url);
+}
+
+const initDB = async () => {
+    await listing.deleteMany({});
+    await listing.insertMany(initData.data);
+}
+
+initDB();
